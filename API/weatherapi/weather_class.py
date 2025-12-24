0563ac69dd36e4bfb09fe78c017ae5fd392ee9a0
llibanm <liban.mohamed-ali@etu.univ-nantes.fr>
2025-12-24 09:21:25 +0100

    created database to store long term weather report data

--- API/weatherapi/weather_class.py
+++ API/weatherapi/weather_class.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+import sqlite3
 
 class weather_report():
 
@@ -23,7 +24,25 @@ class weather_report():
         #API METHOD
         # we will use just 2 for now
         self.current_weather = "/current.json"
-        self.forecast = "/forecast.json"    
+        self.forecast = "/forecast.json"
+
+
+    def sauvegarder_mes_donnees(self):
+
+        # 2. Connexion à la base (crée le fichier 'mes_donnees.db' sur Linux)
+        conn = sqlite3.connect('mes_donnees.db')
+        cursor = conn.cursor()
+
+        # 3. Création de la table (si elle n'existe pas encore)
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS resultats_api (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titre TEXT,
+            valeur TEXT
+        )
+    ''')
+
+         
 
     def build_request(self,city:str,current_weather : bool = True,forecast : bool = False,aqi : bool = False):
         complete_url = ""
@@ -43,8 +62,6 @@ class weather_report():
 if __name__=='__main__':
 
     weather = weather_report()
-    request  = requests.get(weather.build_request('Paris'))
-
-    print(request.json())
+    weather.sauvegarder_mes_donnees()
 
     pass       
